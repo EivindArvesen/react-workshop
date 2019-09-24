bce755351a02355359f3a6cd5e89ecaa06e978bb
Eivind Arvesen <eivind.arvesen@gmail.com>
2019-09-24 21:14:21 +0200

    Add solution for ex 4

--- bouvet-react-app/src/App.jsx
+++ bouvet-react-app/src/App.jsx
@@ -2,33 +2,36 @@ import React from "react";
 
 import Summary from "./Summary";
 import TodoList from "./TodoList";
+import AddTodo from "./AddTodo";
+
+import {themes} from './themeContext';
 
 import "./app.css";
 
-const App = () => (
-  <div className="app">
-    <h1>My Todo App</h1>
-    <Summary todosCount={5} completedTodosCount={3} />
-    <TodoList
-     todoItems={[
-                {
-                    id: "_xss6p50dk",
-                    description: "Check Reddit",
-                    isCompleted: true
-                },
-                {
-                    id: "_alqv4d8mp",
-                    description: "Water plants",
-                    isCompleted: false
-                },
-                {
-                    id: "_s7nx464tr",
-                    description: "Build a todo app in React",
-                    isCompleted: false
-                }
-            ]}
-    />
-  </div>
-);
+const App = () => {
+  const [todos, setTodos] = React.useState([]);
+
+  const addTodo = (id, description) => {
+    const newTodos = [...todos, { id, description, completed: false }];
+    setTodos(newTodos);
+   };
+
+  const removeTodo = id => {
+    setTodos(todos => todos.filter(t => t.id !== id));
+  };
+
+  return (
+	  <div className="app">
+	    <h1>My Todo App</h1>
+	    <Summary todosCount={5} completedTodosCount={3} />
+	    <AddTodo addTodo={addTodo} todos={todos}/>
+	    <TodoList
+				theme={themes.light}
+				todoItems={todos}
+				removeTodo={removeTodo}
+	    />
+	  </div>
+  )
+};
 
 export default App;
--- bouvet-react-app/src/TodoItem.jsx
+++ bouvet-react-app/src/TodoItem.jsx
@@ -1,21 +1,37 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import { ThemeContext } from "./themeContext";
+
 import "./todoItem.css";
 
-const TodoItem = ({ id, description }) => (
-  <div className="todo-item__container">
-    <div>
-      <input type="checkbox" id={`todoItemCheckbox-${id}`} />
-      <label htmlFor={`todoItemCheckbox-${id}`}>{description}</label>
+const TodoItem = ({ description, id, removeTodo }) => {
+  const theme = React.useContext(ThemeContext);
+  return (
+    <div className="todo-item__container">
+      <div>
+        <div className="todo-item__line">
+          <input type="checkbox" id={`todoItemCheckbox-${id}`} />
+          <label htmlFor={`todoItemCheckbox-${id}`}>
+            {description}
+          </label>
+        </div>
+        <button
+          type="button"
+          onClick={() => removeTodo(id)}
+          style={{ backgroundColor: theme.background }}
+        >
+          Delete
+        </button>
+      </div>
     </div>
-    <input type="button" value="Delete" />
-  </div>
-);
+  );
+};
 
 TodoItem.propTypes = {
   description: PropTypes.string.isRequired,
   id: PropTypes.string.isRequired,
+  removeTodo: PropTypes.func.isRequired
 };
 
 export default TodoItem;
